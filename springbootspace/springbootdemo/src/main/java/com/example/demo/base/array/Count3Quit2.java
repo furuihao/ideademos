a885a1165b01dd4bbcdbe35a6b9cb190a48f37b1
furuihao <94402146@qq.com>
2017-11-02 23:18:14 +0800

    逢三退一

---
+++ springbootspace/springbootdemo/src/main/java/com/example/demo/base/array/Count3Quit2.java
@@ -0,0 +1,22 @@
+package com.example.demo.base.array;
+
+/**
+ * 逢三退一
+ */
+public class Count3Quit2 {
+
+
+}
+
+class KidCircle{
+
+}
+
+class Kid {
+    private Kid leftKid;
+    private Kid rightKid;
+
+    public int id;
+
+}
+
