836d9ecaa489f81c2502d9e4b5363a0a13741e98
furuihao <94402146@qq.com>
2018-01-06 12:12:11 +0800

    提交

--- springbootspace/springbootdemo/src/main/java/com/example/demo/service/impl/UserServiceImpl.java
+++ springbootspace/springbootdemo/src/main/java/com/example/demo/service/impl/UserServiceImpl.java
@@ -1,14 +1,21 @@
 package com.example.demo.service.impl;
 
+import com.example.demo.dao.UserMapper;
+import com.example.demo.modules.User;
 import com.example.demo.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Service;
 
+import javax.annotation.Resource;
+import java.util.List;
+
 @Service
 public class UserServiceImpl implements UserService {
     @Autowired
     private JdbcTemplate jdbcTemplate;
+    @Resource
+    private UserMapper userMapper;
 
     @Override
     public void create(String name, Integer age) {
@@ -17,6 +24,7 @@ public class UserServiceImpl implements UserService {
 
     @Override
     public void deleteByName(String name) {
+
         jdbcTemplate.update("delete from USER where NAME = ?", name);
     }
 
@@ -28,5 +36,10 @@ public class UserServiceImpl implements UserService {
     @Override
     public void deleteAllUsers() {
         jdbcTemplate.update("delete from USER");
+    }
+
+    @Override
+    public List<User> findByName(String name) {
+        return userMapper.findByName(name);
     }
 }
